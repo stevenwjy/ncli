3b3ba62ef17ede7d69bd2469548dff17ff1b005b
Steven Wijaya <stevenwjy149@gmail.com>
2023-05-21 22:34:03 +0800

    Fix tests

--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ncli.kit_youtube import extract_video_id
+from ncli.kit_youtube import _extract_video_id
 
 
 class TestYoutube(unittest.TestCase):
@@ -10,10 +10,10 @@ class TestYoutube(unittest.TestCase):
         url2 = f"https://youtu.be/{video_id}"
         url3 = f"https://www.youtube.com/embed/{video_id}"
         url4 = f"https://www.youtube.com/v/{video_id}"
-        self.assertEqual(extract_video_id(url1), video_id)
-        self.assertEqual(extract_video_id(url2), video_id)
-        self.assertEqual(extract_video_id(url3), video_id)
-        self.assertEqual(extract_video_id(url4), video_id)
+        self.assertEqual(_extract_video_id(url1), video_id)
+        self.assertEqual(_extract_video_id(url2), video_id)
+        self.assertEqual(_extract_video_id(url3), video_id)
+        self.assertEqual(_extract_video_id(url4), video_id)
 
 if __name__ == '__main__':
     unittest.main()
